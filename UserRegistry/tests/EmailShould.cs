8aa17fcece55bf45883e35aed6e360ac0d215c8e
fernando.perera <fernando.perera@leanmind.es>
2025-02-17 10:32:50 +0000

    :test_tube: NotBeCreatedIfIsInvalid

---
+++ UserRegistry/tests/EmailShould.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using UserRegistry.domain.errors;
+using UserRegistry.domain.vos;
+
+namespace UserRegistry.tests;
+
+public class EmailShould
+{
+
+    // The email must be valid
+    
+    [Fact]
+    public void NotBeCreatedIfIsInvalid()
+    {
+        const string invalidEmail = "@invalid.com";
+        
+        var function = () => Email.Of(invalidEmail);
+
+        function.Should().Throw<InvalidEmailException>();
+    }
+}
